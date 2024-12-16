5360982078a86f7e55a4c2cf78f9b074224db583
Yircas <Yircas@users.noreply.github.com>
2024-12-16 20:28:10 +0100

    fix: working Drum App

--- src/pages/DrumApp.js
+++ src/pages/DrumApp.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { act } from "react"
 
 const audio = [
     { desc: 'Heater 1', clip: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3', key: 'Q' },
@@ -21,21 +22,8 @@ class DrumApp extends React.Component {
             audio_col: audio
         }
         this.handleKeydown = this.handleKeydown.bind(this)
+        this.setDescription = this.setDescription.bind(this)
     }
-
-    handleKeydown(event) {
-        let audio = this.state.audio_col.filter(item => item.key === String.fromCharCode(event.keyCode))
-        if (audio.length === 0) {
-            return
-        }
-            this.setState({
-                      audio_desc: audio[0].desc
-                    },() => {
-                        console.log('Key pressed: %s', String.fromCharCode(event.keyCode))
-                        console.log('Keycode: %d' , event.keyCode)
-                        console.log('description %s', audio[0].desc)
-                    });
-          }
 
     componentDidMount() {
         console.log('Mounted Drum App') 
@@ -45,12 +33,38 @@ class DrumApp extends React.Component {
         document.removeEventListener("keydown", this.handleKeydown)
     }
 
+    setDescription = (key) => {
+
+        let audio = this.state.audio_col.filter(item => item.key === key)
+
+        if (audio.length === 0) {
+            return
+        }
+
+        this.setState({
+            audio_desc: audio[0].desc
+          },() => {
+              console.log('Key pressed: %s', key)
+              console.log('description %s', audio[0].desc)
+          })
+    }
+
+    handleKeydown(event) {
+        let key = event.key.toUpperCase()
+        const button = document.getElementById(key)
+        if (button) {
+            button.click()
+        }
+    }
+
+    
+
     render() {
         return (
             <div id="drum-machine" className="container mt-4">
                 <h1 className='text-primary text-center mb-4'>Drum Machine</h1>
                 <Display audio_desc={this.state.audio_desc} />
-                <DrumPads handleKeydown={this.handleKeydown} audio_col={this.state.audio_col}/>
+                <DrumPads audio_col={this.state.audio_col} setDescription={this.setDescription} />
             </div>
         )
     }
@@ -67,10 +81,8 @@ class Display extends React.Component {
             <div className="container d-flex flex-column align-items-center">
                 <div className="card shadow p-4 text-center bg-primary-subtle" style={{width: '100%'}}>
                     <div className="card-body">
-                        <div id="display">
                             <h4>Now Playing:</h4>
-                            <p className="text-secondary">{this.props.audio_desc || 'Press a button to play!'}</p>
-                        </div>
+                            <p id='display' className="text-secondary">{this.props.audio_desc || 'Press a button to play!'}</p>
                     </div>
                 </div>
             </div>
@@ -79,18 +91,38 @@ class Display extends React.Component {
 }
 
 class DrumPads extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeKey: null
+        }
+    }
+
     componentDidMount() {
         console.log('Mounted Drum Pads') 
     }
 
+    handleDrumPad(key) {
+        this.props.setDescription(key)
+        this.setState({
+            activeKey: key
+        })
+
+        const audioElement = document.getElementById(key)
+        if (audioElement) {
+          audioElement.cloneNode(true).play() // doesn't pass 5. & 6. Test case
+        // audioElement.play() // default, will pass
+        }
+      }
     
     render() {
         const drumPads = this.props.audio_col.map(i => 
-        <button id={i.desc} 
+        <button 
+        id={i.desc} 
         key={i.key} 
-        className="btn btn-outline-primary m-2 drum-pad" 
+        className={`btn btn-outline-primary m-2 drum-pad ${this.state.activeKey === i.key ? "active" : ""}`}
         style={{ width: '80px', height: '80px' }} 
-        onClick={this.props.handleKeydown}>
+        onClick={() => this.handleDrumPad(i.key)}>
             {i.key}
             <audio id={i.key} src={i.clip} className='clip' />
         </button>);
